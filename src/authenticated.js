e2ec36c4c2b88f82d2e00291ec441aa96e98c8d3
Alexey <Alexey>
2026-08-21 11:58:23 +0300

    remove auth token when token is epired

--- src/authenticated.js
+++ src/authenticated.js
@@ -4,11 +4,16 @@ import { AuthToken } from '@/authToken.js'
 export function authenticated() {
   var authToken = storage.getAuthToken();
 
-
   if (authToken) {
     authToken = new AuthToken(storage.getAuthToken());
 
-    return !authToken.isExpired();
+    if (authToken.isExpired()) {
+      // Remove expired token and request new one
+      storage.removeAuthToken();
+      return false;
+    }
+
+    return true;
 
   } else {
     return false;
--- src/storage.js
+++ src/storage.js
@@ -7,6 +7,22 @@ class Storage {
     return localStorage.getItem('authToken');
   }
 
+  removeAuthToken() {
+    localStorage.removeItem('authToken');
+  }
+
+  saveRefreshToken(refreshToken) {
+    localStorage.setItem('refreshToken', refreshToken);
+  }
+
+  getRefreshToken() {
+    return localStorage.getItem('refreshToken');
+  }
+
+  removeRefreshToken() {
+    localStorage.removeItem('refreshToken');
+  }
+
   save(item, data) {
     localStorage.setItem(item, data);
   }
